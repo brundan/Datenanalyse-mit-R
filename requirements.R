39bb98ead0aeb144679604fda7e280f74ca072c9
brundan <brunner.da@gmail.com>
2018-04-05 17:07:25 +0200

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,9 +1,9 @@
 library(remotes)
-library(dplyr)
+
 ##### Install specific package versions from CRAN #####
 
 # install_version("ggplot2", "2.2.1")
-# install_version("dplyr")
+ install_version("dplyr")
 # install_version("tidyr", "0.8.0")
 # install_version("purrr", "0.2.4")
 
